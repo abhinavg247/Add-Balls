556cdde3b15e6b991ea3eb19d141fcee98e12e76
Abhishek Garg <31281299+abhiishekgarg@users.noreply.github.com>
2020-10-04 01:43:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var colors = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick', 'crimson'];
 var capacity = 32;
 var balls = 0;
+const initial = $("#container").height();
 
 $("button").on("click", function()
 {
     if(balls == capacity)
     {
-        capacity *= 2;
-        var h = $("#container").height();
-        $("#container").height(2*h);
+        capacity += 32;
+        var h = $("#container").height() + initial;
+        $("#container").height(h);
     }
     balls++;
     var colorball = colors[Math.floor(Math.random()*colors.length)];
